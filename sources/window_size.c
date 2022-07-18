3c15c4a63ae492c4dc0ca39d5cf017cb9fc6db1d
Max Rantil <rantil@pm.me>
2022-07-18 11:55:57 +0000

    window_size

--- sources/window_size.c
+++ sources/window_size.c
@@ -1,28 +1,14 @@
 #include "ft_ls.h"
 #include <sys/ioctl.h>
-#include <termios.h>
-
-static void pr_winsize(int fd)
-{
-	struct winsize	size;
-
-	if (ioctl(fd, TIOCGWINSZ, (char *)&size) < 0)
-		perror("TIOCGWINSZ");
-	ft_printf("%d: rows, %d: colums\n", size.ws_row, size.ws_col);
-}
-
-static void	sig_winch(int signo)
-{
-	ft_printf("SIGWINCH recieved\n");
-	pr_winsize(STDIN_FILENO);
-}
 
 int	window_size(void)
 {
-	if (isatty(STDIN_FILENO) == 0)
-		exit(1);
-	if (signal(SIGWINCH, sig_winch) == SIG_ERR)
-		perror("signal");
-	pr_winsize(STDIN_FILENO);
+	struct winsize	size;
+	int	fd;
+
+	fd = STDIN_FILENO;
+	if (ioctl(fd, TIOCGWINSZ, (char *)&size) < 0)
+		perror("TIOCGWINSZ");
+	ft_printf("%d: rows, %d: colums\n", size.ws_row, size.ws_col);
 	return (1);
 }
